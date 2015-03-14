6521e34ee4ffa5174a2b9b2beb66410edb41c05d
Zachary J. Fields <zachary_fields@yahoo.com>
2015-03-14 02:24:42 -0700

    Refactored default _control_register_address array load
    Unit-test passed

--- firmware/mcp23s17.cpp
+++ firmware/mcp23s17.cpp
@@ -15,12 +15,9 @@
 mcp23s17::mcp23s17 (
 	const HardwareAddress hw_addr_
 ) :
-	_SPI_BUS_ADDRESS(0x40 | (hw_addr_ << 1))
+	_SPI_BUS_ADDRESS(0x40 | (hw_addr_ << 1)),
+	_control_register_address{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 }
 {
-	for ( int i = 0 ; i < REGISTER_COUNT ; ++i ) {
-		_control_register_address[i] = i;
-	}
-
 	return;
 }
 
