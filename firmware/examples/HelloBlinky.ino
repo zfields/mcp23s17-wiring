93ec3d6f8ae7649b3ef234fa56b5ce4228f74d1c
Zachary J. Fields <zachary_fields@yahoo.com>
2015-04-19 21:41:29 -0700

    Added HelloBlinky.ino example

---
+++ firmware/examples/HelloBlinky.ino
@@ -0,0 +1,19 @@
+/****************/
+ /* Hello Blinky */
+/****************/
+#include "mcp23s17/mcp23s17.h"
+
+const int LED_PIN = 7;
+mcp23s17 gpio_x(mcp23s17::HardwareAddress::HW_ADDR_0); // All addressing pins set to GND
+
+void setup (void) {
+    gpio_x.pinMode(LED_PIN, mcp23s17::PinMode::OUTPUT);
+}
+
+void loop (void) {
+    gpio_x.digitalWrite(LED_PIN, HIGH);
+    delay(500);
+    gpio_x.digitalWrite(LED_PIN, LOW);
+    delay(500);
+}
+
